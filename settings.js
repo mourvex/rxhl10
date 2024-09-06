45e2f7dadee2f818ec4ed4679528c2046674a6c8
mourvex <mourvext@gmail.com>
2024-09-06 13:17:58 +0700

    rxhl10

---
+++ settings.js
@@ -0,0 +1,7 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['6287792700432']
+global.botname = 'DevilsAdvocate Bot'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "SilumanBaskom"
+global.sticker2 = "🌜"
